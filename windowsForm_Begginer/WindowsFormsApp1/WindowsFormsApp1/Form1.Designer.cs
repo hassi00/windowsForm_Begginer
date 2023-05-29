b7201441c788ca254d1b4d8fec5badd3357617f2
hassi00 <hasibiz5566@gmail.com>
2023-05-29 23:10:22 +0900

    四則演算処理の作成

--- windowsForm_Begginer/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ windowsForm_Begginer/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.cmbOperator = new System.Windows.Forms.ComboBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(392, 229);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "計算実行";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(238, 177);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(100, 19);
+            this.txtOperand1.TabIndex = 1;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(520, 175);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(100, 19);
+            this.txtOperand2.TabIndex = 2;
+            // 
+            // cmbOperator
+            // 
+            this.cmbOperator.FormattingEnabled = true;
+            this.cmbOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cmbOperator.Location = new System.Drawing.Point(370, 175);
+            this.cmbOperator.Name = "cmbOperator";
+            this.cmbOperator.Size = new System.Drawing.Size(121, 20);
+            this.cmbOperator.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(405, 277);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(53, 12);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "計算結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.cmbOperator);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.ComboBox cmbOperator;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- windowsForm_Begginer/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ windowsForm_Begginer/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,36 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtOperand1.Text) && !string.IsNullOrWhiteSpace(txtOperand2.Text))
+            {
+                double operand1 = double.Parse(txtOperand1.Text);
+                double operand2 = double.Parse(txtOperand2.Text);
+                string operatorSymbol = cmbOperator.SelectedItem.ToString();
+
+                double result = 0;
+
+                switch (operatorSymbol)
+                {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                    case "/":
+                        result = operand1 / operand2;
+                        break;
+                }
+
+                lblResult.Text = result.ToString();
+            }
+
+        }
     }
 }
