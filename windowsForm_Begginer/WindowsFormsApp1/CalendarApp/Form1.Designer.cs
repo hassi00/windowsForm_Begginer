2d4cc99687e04872bd32abcf5e3cdf2d73559b83
hassi00 <hasibiz5566@gmail.com>
2023-06-13 00:11:19 +0900

    カレンダアプリを作成

---
+++ windowsForm_Begginer/WindowsFormsApp1/CalendarApp/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CalendarApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(80, 38);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(80, 229);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(199, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(80, 359);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "追加";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(204, 359);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "削除";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 450);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ windowsForm_Begginer/WindowsFormsApp1/CalendarApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CalendarApp
+{
+    public partial class Form1 : Form
+    {
+
+        private Dictionary<DateTime, string> schedule;
+
+        public Form1()
+        {
+            InitializeComponent();
+            schedule = new Dictionary<DateTime, string>();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                return;
+            }
+
+            if (schedule.ContainsKey(selectedDate))
+            {
+                schedule[selectedDate] = richTextBox1.Text;
+            }
+            else
+            {
+                schedule.Add(selectedDate, richTextBox1.Text);
+            }
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+            if (schedule.ContainsKey(selectedDate))
+            {
+                richTextBox1.Text = schedule[selectedDate];
+            }
+            else
+            {
+                richTextBox1.Text = "";
+            }
+        }
+
+        private void buttonDelete_Click(object sender, MouseEventArgs e)
+        {
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+            if (schedule.ContainsKey(selectedDate))
+            {
+                schedule.Remove(selectedDate);
+                richTextBox1.Text = "";
+            }
+        }
+    }
+}
